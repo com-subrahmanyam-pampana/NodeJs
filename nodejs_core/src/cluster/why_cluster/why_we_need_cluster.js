fbbd738f5a67363d8a159edf2347929afa068097
com-subrahmanyam-pampana <130865446+com-subrahmanyam-pampana@users.noreply.github.com>
2024-02-18 12:19:28 -0800

    cluster added

--- nodejs_core/src/cluster/why_cluster/why_we_need_cluster.js
+++ nodejs_core/src/cluster/why_cluster/why_we_need_cluster.js
@@ -12,10 +12,11 @@ app.get("/fast", (req, res) => {
 });
 
 function load(time) {
-  /*Note this is a sync operation and it is done by Main Thread 
+  /*Note this is a sync and a non-blocking operation operation and it is done by Main Thread 
   of JS */
   /*JS code. */
+   /* the event loop will get stuck until the operation finished */
   const start = Date.now();
   while (Date.now() - start < time) {}
 }
-app.listen(5000);
+app.listen(3000);
